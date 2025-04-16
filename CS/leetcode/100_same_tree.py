7631573552a53bdc088c36145bdc4c2bbe96f755
Gumi_Lockhart <gumi.200010@gmail.com>
2025-04-16 11:02:16 +0800

    restruct

---
+++ CS/leetcode/100_same_tree.py
@@ -0,0 +1,39 @@
+"""
+Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+"""
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: Optional[TreeNode]
+        :type q: Optional[TreeNode]
+        :rtype: bool
+        """
+
+        # recursive solution
+        def isSame(node1, node2):
+            if node1 is None:
+                if node2 is None:
+                    return True
+                else:
+                    return False
+            if node2 is None:
+                if node1 is None:
+                    return True
+                else:
+                    return False
+            if node1.val != node2.val:
+                return False
+            else:
+                return isSame(node1.left, node2.left) and isSame(node1.right, node2.right)
+        return isSame(p, q)
+
